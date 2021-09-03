04deb221171dfdef86c62b834221e345d3346e3a
Jose Klaumann <jose.augusto.klaumann@hotmail.com>
2021-09-02 22:26:46 -0300

    Actual change with notes

--- src/date/Program.java
+++ src/date/Program.java
@@ -18,24 +18,24 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-		Date firstDate = sdf.parse(sc.nextLine());
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm"); //Formata e analisa conforme o padrao especificado
+		Date firstDate = sdf.parse(sc.nextLine()); //Digitar a data desejada
 		Date secondDate = sdf.parse(sc.nextLine());
 
-		long diffrenceH = 0, diffrenceM = 0;
-
-		long diff = secondDate.getTime() - firstDate.getTime();
-
-		TimeUnit timeHo = TimeUnit.HOURS;
-		diffrenceH = timeHo.convert(diff, TimeUnit.MILLISECONDS);
-		System.out.println("The difference in hours is : " + diffrenceH);
-		diffrenceH %= 24;
+		long diff = secondDate.getTime() - firstDate.getTime(); //As datas sao convertidas para milisegudos e aqui ele faz o calculo da diferenca
 		
-		TimeUnit timeMi = TimeUnit.MINUTES;
-		diffrenceM = timeMi.convert(diff, TimeUnit.MILLISECONDS);
+		//O método convert() da classe TimeUnit aceita dois parâmetros, que são duração do tempo e unidade da duração do tempo.
+		
+		TimeUnit timeHo = TimeUnit.HOURS; //Define que a conversao deve ser feita para horas
+		long diffrenceH = timeHo.convert(diff, TimeUnit.MILLISECONDS); //Transforma os milisegundos em horas
+		System.out.println("The difference in hours is : " + diffrenceH); 
+		diffrenceH %= 24; //Caso de um valor maior ele utiliza este calculo e pega o resto da divisao como resposta
+		
+		TimeUnit timeMi = TimeUnit.MINUTES;//Define que a conversao deve ser feita para minutos
+		long diffrenceM = timeMi.convert(diff, TimeUnit.MILLISECONDS);//Transforma os milisegundos em minutos
 
 		System.out.println("The difference in min is : " + diffrenceM);
-		diffrenceM %= 60;
+		diffrenceM %= 60;//Caso de um valor maior ele utiliza este calculo e pega o resto da divisao como resposta
 
 		System.out.printf("O JOGO DUROU %s HORA(S) E %s MINUTO(S)", diffrenceH, diffrenceM);
 		sc.close();
